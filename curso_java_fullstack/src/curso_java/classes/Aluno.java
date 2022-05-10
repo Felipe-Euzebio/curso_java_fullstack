216c7fd447288e705f5258df6767ff92d534cb37
Felipe <99846344+Felipe-Euzebio@users.noreply.github.com>
2022-05-10 15:59:22 -0300

    Criando métodos getters e setters.

--- curso_java_fullstack/src/curso_java/classes/Aluno.java
+++ curso_java_fullstack/src/curso_java/classes/Aluno.java
@@ -14,6 +14,7 @@ public class Aluno {
 	String instituicaoEnsino;
 	String serie;
 	
+	/*
 	public Aluno(String nome, int idade, String nomeResponsavel, int matricula) {
 		super();
 		this.nome = nome;
@@ -21,7 +22,81 @@ public class Aluno {
 		this.nomeResponsavel = nomeResponsavel;
 		this.matricula = matricula;
 	}
-		
+	*/
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNomeResponsavel() {
+		return nomeResponsavel;
+	}
+
+	public void setNomeResponsavel(String nomeResponsavel) {
+		this.nomeResponsavel = nomeResponsavel;
+	}
+
+	public int getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getInstituicaoEnsino() {
+		return instituicaoEnsino;
+	}
+
+	public void setInstituicaoEnsino(String instituicaoEnsino) {
+		this.instituicaoEnsino = instituicaoEnsino;
+	}
+
+	public String getSerie() {
+		return serie;
+	}
+
+	public void setSerie(String serie) {
+		this.serie = serie;
+	}
+	
+			
 }
 
 
--- curso_java_fullstack/src/curso_java/executavel/MinhaClasse.java
+++ curso_java_fullstack/src/curso_java/executavel/MinhaClasse.java
@@ -6,9 +6,11 @@ public class MinhaClasse {
 
 	public static void main(String[] args) {
 		
-		Aluno aluno1 = new Aluno("Felipe", 19, "Rômulo", 2021);
-		Aluno aluno2 = new Aluno("Tiago", 17, "Rômulo", 2022);
+		// Aluno alunoTeste = new Aluno("Felipe", 19, "Rômulo", 2021);
+		Aluno aluno1 = new Aluno();
+		aluno1.setNome("João");
 
+		System.out.println("Aluno 1: " + aluno1.getNome());
 
 	}
 
