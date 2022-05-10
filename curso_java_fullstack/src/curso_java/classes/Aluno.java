3111a33b1685999c58764ea5c8781b901d42f69e
Felipe <99846344+Felipe-Euzebio@users.noreply.github.com>
2022-05-10 02:11:12 -0300

    Minha primeira classe em Java.

--- curso_java_fullstack/src/curso_java/classes/Aluno.java
+++ curso_java_fullstack/src/curso_java/classes/Aluno.java
@@ -1,10 +1,27 @@
 package curso_java.classes;
 
+import java.util.Date;
+
 public class Aluno {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	String nome;
+	int idade;
+	Date dataNascimento;
+	String rg;
+	String cpf;
+	String nomeResponsavel;
+	int matricula;
+	String instituicaoEnsino;
+	String serie;
+	
+	public Aluno(String nome, int idade, String nomeResponsavel, int matricula) {
+		super();
+		this.nome = nome;
+		this.idade = idade;
+		this.nomeResponsavel = nomeResponsavel;
+		this.matricula = matricula;
 	}
-
+		
 }
+
+
---
+++ curso_java_fullstack/src/curso_java/executavel/MinhaClasse.java
@@ -0,0 +1,15 @@
+package curso_java.executavel;
+
+import curso_java.classes.Aluno;
+
+public class MinhaClasse {
+
+	public static void main(String[] args) {
+		
+		Aluno aluno1 = new Aluno("Felipe", 19, "Rômulo", 2021);
+		Aluno aluno2 = new Aluno("Tiago", 17, "Rômulo", 2022);
+
+
+	}
+
+}
